11ac59ebdacbf4db2dded9802fbe8f987d626814
J-osu <josuedelacr96@gmail.com>
2026-01-19 10:41:33 +0100

    More...

--- components/admin-dashboard.tsx
+++ components/admin-dashboard.tsx
@@ -254,7 +254,7 @@ export interface Product {
                         <SelectContent>
                           {categories.map((category) => (
                             <SelectItem key={category.id} value={category.id}>
-                              {category.name} (ID: {category.id})
+                            (ID: {category.id}) {category.name} 
                             </SelectItem>
                           ))}
                         </SelectContent>
